b5e8cc909cae9c14a616e6acb25c98f03d00e64b
gjx7598189 <2844583892@qq.com>
2017-12-06 16:23:05 +0800

    将Logs文件推送到远程

--- ihome/api_v1_0/index.py
+++ ihome/api_v1_0/index.py
@@ -2,10 +2,14 @@
 from . import api
 from flask import session
 from ihome import redis_store
-
+from flask import current_app
+import logging
 
 @api.route('/index',methods=["GET","POST"])
 def index():
     session["name"] = "xiaohua"
     redis_store.set('name','xiaohei')
+    logging.debug("DEBUG LOGLOG")
+    print ("===========================")
+    current_app.logger.debug("DEBUG LOGLOGLOG CURRENT")
     return 'index'
