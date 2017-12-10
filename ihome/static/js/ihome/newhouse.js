2c20f83cdcffda00740a272b5a9d845b4df1f739
gjx7598189 <2844583892@qq.com>
2017-12-10 17:06:52 +0800

    使用前端的模板语言进行显示html页面

--- ihome/static/js/ihome/newhouse.js
+++ ihome/static/js/ihome/newhouse.js
@@ -10,12 +10,14 @@ $(document).ready(function(){
     // 在页面加载完毕之后获取区域信息
     $.get("/api/v1.0/areas",function (resp) {
         if(resp.erron=="0"){
-            for(var i=0;i<resp.data.areas.length;i++){
-                var areaid = resp.data.areas[i].aid
-                var areaname = resp.data.areas[i].aname
-                // 将选项添加到select中
-                $("#area-id").append('<option value="'+ areaid +'">'+ areaname + '</option>>')
-            }
+            // for(var i=0;i<resp.data.areas.length;i++){
+            //     var areaid = resp.data.areas[i].aid
+            //     var areaname = resp.data.areas[i].aname
+            //     // 将选项添加到select中
+            //     $("#area-id").append('<option value="'+ areaid +'">'+ areaname + '</option>>')
+            // }
+            var html = template("areas-tmpl",{"areas":resp.data.areas})
+            $("#area-id").html(html)
         }
     })
     // TODO: 处理房屋基本信息提交的表单数据
