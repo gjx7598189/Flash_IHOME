d8bc2c29839e411a267a5b4cfeb3c5dab9f99e8e
gjx7598189 <2844583892@qq.com>
2017-12-10 16:55:07 +0800

    将城区信息缓存到redis中

--- ihome/api_v1_0/house.py
+++ ihome/api_v1_0/house.py
@@ -1,12 +1,24 @@
 # -*- coding:utf-8 -*-
 
-from flask import request,g,jsonify
+from flask import request,g,jsonify,current_app
 from ihome.models import Area,Facility,User
 from . import api
 from ihome.utils.response_code import RET
+from ihome import redis_store
+from ihome.constants import AREA_INFO_REDIS_EXPIRES
+
 
 @api.route("/areas")
 def get_areas():
+
+    # 先从redis中获取城区信息
+    try:
+        areas_array = redis_store.get("areas")
+        if areas_array:
+            return jsonify(erron=RET.OK,errmsg="OK",data={"areas":eval(areas_array)})
+    except Exception as e:
+        current_app.logger.error(e)
+
 
     # 获取所有的城区信息
     areas = Area.query.all()
@@ -15,6 +27,12 @@ def get_areas():
     areas_array = []
     for area in areas:
         areas_array.append(area.to_dict())
+
+    # 将数据缓存到redis中
+    try:
+        redis_store.set("areas",areas_array,AREA_INFO_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
 
     # 数据返回
     return jsonify(erron=RET.OK,errmsg="OK",data={"areas":areas_array})
